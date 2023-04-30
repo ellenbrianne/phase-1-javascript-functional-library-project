fbd6de23f809c2b64a0801d868583f9c0282971e
Ellen Bennett <ellenbennett@Ellens-MacBook-Air.local>
2023-04-30 14:10:56 -0600

    successfully completed collection functions

---
+++ index.js
@@ -0,0 +1,125 @@
+function convertValues (object) {
+    let valArray = Object.values(object)
+    return valArray
+}
+
+function convertKeys (object) {
+    let keysArray = Object.keys(object)
+    return keysArray
+}
+
+// Collection Functions
+
+function myEach (collection, callback) {
+    let objValues
+
+    if (typeof collection === "object") {
+        objValues = convertValues(collection)
+    } else {
+        objValues = collection
+    }
+    for (let item of objValues) {
+        callback(item)
+    } 
+    return collection
+}
+
+function myMap (collection, callback) {
+    let objValues
+    let newArr = []
+
+    if (typeof collection === "object") {
+        objValues = convertValues(collection)
+    } else {
+        objValues = collection
+    }
+    for (let item of objValues) {
+       newArr.push(callback(item))
+    }
+    return newArr
+}
+
+function myReduce (collection, callback, acc) {
+    let objValues 
+
+    if (typeof collection === "object") {
+        objValues = convertValues(collection)
+    } else {
+        objValues = collection
+    }
+
+    if (acc === undefined) {
+        acc = objValues[0]
+        for (let i = 1; i < objValues.length; i++) {
+            acc = callback(acc, objValues[i], objValues)
+        }
+    } else {
+        for (let j = 0; j < objValues.length; j++) {
+            acc = callback(acc, objValues[j], objValues)
+        }
+    }
+    return acc
+}
+
+function myFind (collection, predicate) {
+    let objValues 
+    let finalVal = undefined
+
+    if (typeof collection === "object") {
+        objValues = convertValues(collection)
+    } else {
+        objValues = collection
+    }
+
+    for (let i = 0; i < objValues.length; i++) {
+        let value = predicate(objValues[i])
+        if (value === true) {
+            return finalVal = objValues[i]
+        }
+    }
+    return finalVal
+}
+
+function myFilter (collection, predicate) {
+    let objValues 
+    let finalValues = []
+
+    if (typeof collection === "object") {
+        objValues = convertValues(collection)
+    } else {
+        objValues = collection
+    }
+
+    for (let i = 0; i < objValues.length; i++) {
+        let value = predicate(objValues[i])
+        if (value === true) {
+            finalValues.push(objValues[i])
+        }
+    }
+    return finalValues
+}
+
+function mySize (collection) {
+    let finalValue
+
+    if (typeof collection === "object") {
+       return finalValue = Object.keys(collection).length
+    }
+
+    finalValue = collection.length 
+    return finalValue
+}
+
+// Array Functions
+
+function myFirst (array, n) {
+    let modArr 
+    if (n) {
+        modArr = array.slice(0, n)
+    } else {
+        modArr = array.shift()
+    }
+    return modArr
+}
+
+console.log(myFirst([1, 2, 3, 4, 5]))
